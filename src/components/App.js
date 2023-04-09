b838603db2f349f17a66da003b6e88b1b9a72170
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-04-09 15:50:23 -0500

    Complete project

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,51 @@
 import React, { Component } from 'react';
+import Overview from './Overview';
+
+//      render input field
+//      render submit button
+//      on submit... add input to stateful tasks array
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            array: [],
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+        this.setState({value: e.target.value});
+    }
+    handleSubmit(e) {
+        console.log(this.state.value.split(', '));
+        e.preventDefault();
+        this.setState({submit: true});
+        this.setState({array: this.state.value.split(', ')})
+    }
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        To do tasks:
+                        <input type='text'
+                            name='tasks'
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            placeholder='separate tasks by ", " (comma + space)'>
+                        </input>
+                    </label>
+                    <button>Submit</button>
+                </form>
+                <Overview array={this.state.array}/>
+            </>
+        )
+    }
+}
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,3 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+//      render each tasks in stateful tasks array as item in list
+
+class Overview extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleDeleteClick(e) {
+        const index = e.target.id;
+        this.setState({array: this.props.array.splice(index, 1)})
+    }
+
+    render() {
+        const items = this.props.array.map((item, index) => 
+            <>
+                <li key={index}>{index + 1}. {item}</li>
+                <button id={index} onClick={this.handleDeleteClick}>X</button>
+                <button id={index} onClick={this.handleEditClick}>Edit</button>
+            </>
+        )
+        return (
+            <ul>
+                {items}
+            </ul>
+        )
+    }
+}
+Overview.propTypes = {
+    array: PropTypes.array.isRequired,
+    submit: PropTypes.bool.isRequired,
+  }
 
 export default Overview;
